c3a5732867ef8c415c52f3dc9b1be4773861101d
unkerpete <peterkjx@gmail.com>
2023-02-28 22:26:12 +0800

    npm i jsonwebtoken bcrypt, and drafted users table

--- database.sql
+++ database.sql
@@ -4,6 +4,7 @@ CREATE DATABASE toilets_app;
 
 CREATE TYPE bidet AS ENUM ('no', 'manual', 'automatic');
 CREATE TYPE sex AS ENUM ('male', 'female', 'unisex');
+CREATE TYPE role AS ENUM ('user', 'admin');
 
 CREATE TABLE toilets(
   id SERIAL PRIMARY KEY,
@@ -14,4 +15,12 @@ CREATE TABLE toilets(
   bidet bidet NOT NULL,
   _address VARCHAR(255) NOT NULL,
   postalcode VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE users(
+  id SERIAL,
+  username VARCHAR(50) UNIQUE NOT NULL,
+  email VARCHAR(255) UNIQUE PRIMARY KEY NOT NULL,
+  _password VARCHAR(255) NOT NULL,
+  _role role default 'user'
 );
